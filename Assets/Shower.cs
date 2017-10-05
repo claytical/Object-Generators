335e4699aa9e9bc17441dc205aaabcb1bbd9b072
Clay Ewing <c.ewing@miami.edu>
2017-10-04 20:07:49 -0400

    init commit

---
+++ Assets/Shower.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shower : MonoBehaviour {
+	public GameObject item;
+	public int minimumAmount;
+	public int maximumAmount;
+	public float timeBetweenShowers;
+	private float timeSinceLastShower;
+	// Use this for initialization
+	void Start () {
+		timeSinceLastShower = Time.time + timeBetweenShowers;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeSinceLastShower <= Time.time) {
+			timeSinceLastShower = Time.time + timeBetweenShowers;
+			int numberOfItemsToDrop = Random.Range (minimumAmount, maximumAmount);
+			for(int i = 0; i < numberOfItemsToDrop; i++) {
+				GameObject showerItem = (GameObject)Instantiate (item, transform);
+				showerItem.transform.Translate (Random.Range (-.01f, .01f), Random.Range (-.01f, .01f), Random.Range (-.01f, .01f)); 
+				showerItem.GetComponent<CollectableItem> ().value = Random.Range (1, 100);
+			}
+		}
+	}
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.tag == "Weapon") {
+			Renderer r = gameObject.GetComponent<Renderer>();
+			r.material.shader = Shader.Find ("Standard");
+			r.material.SetColor ("_Color", Color.red);
+		}
+	}
+}
